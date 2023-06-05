a40136bd52dfe5051118e24e42d04dbd56e5154a
Patrick Wulfe <wulfep@gmail.com>
2023-06-04 17:35:24 -0700

    Finished initial implementation of card search

--- lib/modules/app/view/home_page/home_page.dart
+++ lib/modules/app/view/home_page/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:gap/gap.dart';
 import 'package:sideboard/modules/app/app_index.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,13 +9,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pink,
       child: Center(
-        child: FilledButton(
-          child: const Text('Go to chat'),
-          onPressed: () => Modular.to.navigate(
-            AppRoutes.deckGenerator.name + '/',
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FilledButton(
+              child: const Text('Go to chat'),
+              onPressed: () => Modular.to.navigate(
+                '${AppRoutes.deckGenerator.name}/',
+              ),
+            ),
+            const Gap(8),
+            FilledButton(
+              child: const Text('Go to search'),
+              onPressed: () => Modular.to.navigate(
+                '${AppRoutes.cardSearch.name}/',
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/modules/card_search/view/mobile_card_search_view.dart
+++ lib/modules/card_search/view/mobile_card_search_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:sideboard/modules/card_search/bloc/card_search_bloc.dart';
 
-class MobileCardSearchView extends StatelessWidget {
+class MobileCardSearchView extends HookWidget {
   const MobileCardSearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final searchTextController = useTextEditingController();
+    final bloc = context.read<CardSearchBloc>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Card Search'),
@@ -16,31 +20,38 @@ class MobileCardSearchView extends StatelessWidget {
           builder: (context, state) {
             return Column(
               children: [
-                const SearchBar(),
-                state.when(
-                  initial: () => const Center(
-                    child: Text('Search for a card'),
-                  ),
-                  loading: () => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  loaded: (cards, selectedIndex) => ListView.builder(
-                    itemCount: cards.length,
-                    itemBuilder: (context, index) {
-                      final card = cards[index];
-                      return ListTile(
-                        title: Text(card.name),
-                        subtitle: Text(card.setName),
-                        onTap: () {
-                          context
-                              .read<CardSearchBloc>()
-                              .add(CardSearchEvent.select(index));
-                        },
-                      );
-                    },
-                  ),
-                  error: (failure) => Center(
-                    child: Text(failure.toString()),
+                const Gap(16),
+                SearchBar(
+                  controller: searchTextController,
+                  hintText: 'Search for a card',
+                  onChanged: (input) => bloc.add(CardSearchEvent.search(input)),
+                ),
+                Expanded(
+                  child: state.when(
+                    initial: () => const Center(
+                      child: Text('Your results will appear here'),
+                    ),
+                    loading: () => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    loaded: (cards, selectedIndex) => ListView.builder(
+                      itemCount: cards.length,
+                      itemBuilder: (context, index) {
+                        final card = cards[index];
+                        return ListTile(
+                          title: Text(card.name),
+                          subtitle: Text(card.setName),
+                          onTap: () {
+                            context
+                                .read<CardSearchBloc>()
+                                .add(CardSearchEvent.select(index));
+                          },
+                        );
+                      },
+                    ),
+                    error: (failure) => Center(
+                      child: Text(failure.toString()),
+                    ),
                   ),
                 ),
               ],
